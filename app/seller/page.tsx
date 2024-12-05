19af4431d34bedba4a4a64ed75069f6272c637a3
mstout60 <mstout@eprospector.com>
2024-12-05 11:26:33 -0600

    Implement Seller Dashboard

---
+++ app/seller/page.tsx
@@ -0,0 +1,16 @@
+import { redirect } from "next/navigation";
+
+import { auth } from "@clerk/nextjs/server";
+
+import SellerDashboard from "@/components/seller-dashboard";
+
+export default async function SellerPage() {
+  const { userId } = await auth();
+  if (!userId) redirect("/");
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <SellerDashboard />
+    </div>
+  );
+}
---
+++ components/seller-dashboard.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+function SellerDashboard() {
+  return <div>SellerDashboard</div>;
+}
+
+export default SellerDashboard;
